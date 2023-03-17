de420d87002b08b4655fc2003170d8d3efc21326
vinayak tavatam <107667781+tavatamvinayak@users.noreply.github.com>
2023-03-17 18:43:50 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,8 @@
+
+function salary() {
+  let x = document.getElementById("anual-salary").value;
+  document.getElementById("demo-1").innerHTML = y = x / 12;
+  document.getElementById("demo-2").innerHTML = z = y / 24;
+  document.getElementById("demo-3").innerHTML = a = z / 8;
+  document.getElementById("demo-4").innerHTML = b = a / 60;
+}
